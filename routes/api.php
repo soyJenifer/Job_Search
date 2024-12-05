4dd6772fd17e4b84cbbb60b84b6d14bede6b2d04
soyJeni <jenifer2006@gmail.com>
2024-12-05 14:28:25 +0100

    feat: Added API

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\OfferController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/offers', [OfferController::class, 'index'])->name('apindex');
+Route::post('/offers', [OfferController::class, 'store'])->name('apistore');
+Route::get('/offers/{id}', [OfferController::class, 'show'])->name('apishow');
+Route::put('/offers/{id}', [OfferController::class, 'update'])->name('apiupdate');
+Route::delete('/offers/{id}', [OfferController::class, 'destroy'])->name('apidestroy');
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OfferController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [OfferController::class, 'index'])->name('index');
+Route::get('/offers/{id}', [OfferController::class, 'show'])->name('show');
