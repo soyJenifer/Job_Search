5693e6b36364e222fe1a675a23cbf023fcc03551
soyJeni <jenifer2006@gmail.com>
2024-12-05 12:59:25 +0100

    feat: blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Trabajo</th>
+        <th scope="col">Descripcion</th>
+        <th scope="col">Empresa</th>
+        <th scope="col">Estado</th>
+        <th scope="col">Fecha</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($offers as $offer)
+        <tr>
+          <td>{{ $offer->id }}</td>
+          <td>{{ $offer->offer }}</td>
+          <td>{{ $offer->description }}</td>
+          <td>{{ $offer->company }}</td>
+          <td>{{ $offer->status }}</td>
+          <td>{{ $offer->created_at }}</td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
+@endsection
