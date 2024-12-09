78ddc49117c0397d75de055882adfc5507215e8c
soyJeni <jenifer2006@gmail.com>
2024-12-09 18:07:41 +0100

    feat: feedback routes

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,9 @@ Route::post('/offers', [OfferController::class, 'store'])->name('apistore');
 Route::get('/offers/{id}', [OfferController::class, 'show'])->name('apishow');
 Route::put('/offers/{id}', [OfferController::class, 'update'])->name('apiupdate');
 Route::delete('/offers/{id}', [OfferController::class, 'destroy'])->name('apidestroy');
+
+Route::get('/offers/{offerId}/news', [FeedbackController::class, 'index'])->name('apiindexNews');
+Route::post('/offers/{offerId}/news', [FeedbackController::class, 'store'])->name('apistoreNews');
+Route::get('/offers/{offerId}/news/{newsID}', [FeedbackController::class, 'show'])->name('apishowNews');
+Route::put('/offers/{offerId}/news/{newsID}', [FeedbackController::class, 'update'])->name('apiupdateNews');
+Route::delete('/offers/{offerId}/news/{newsId}', [FeedbackController::class, 'destroy'])->name('apidestroyNews');
