d4f3a8d0aaacdd65a429e03fd9d5bc0de4ab4e51
soyJeni <jenifer2006@gmail.com>
2024-12-10 15:24:10 +0100

    tests: test coverage-report & testing finished

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -10,7 +10,7 @@ class OfferController extends Controller
 {
     public function index(Request $request)
     {
-        if ($request->action === 'delete')
+        /*if ($request->action === 'delete')
         {
             $this->destroy($request->id);
             return Redirect::to(route('index'));
@@ -25,8 +25,73 @@ class OfferController extends Controller
             $this->edit($request->id, 1);
             return Redirect::to(route('index'));
         }
+            */
         $offers = Offer::all();
         return view('index', compact('offers'));
 
     }
+    /**
+     * Display a listing of the resource.
+     */
+    
+        //
+    
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    /*public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    /*public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $offer = Offer::find($id);
+
+        if ($offer)
+        return view ('show', compact('offer'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    /*
+     public function edit(string $id, bool $status)
+    {
+
+        Offer::find($id)->update(['status' => $status]);
+    }
+    */
+    /**
+     * Update the specified resource in storage.
+     */
+    /*public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    /*
+    public function destroy(string $id)
+    {
+        $offer = Offer::find($id);
+        if($offer)
+        {
+            $offer->delete();
+        }
+    }*/
 }
